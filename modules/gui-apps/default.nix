343d54b676f88cf22e7e0dcd890d066f994d8e7d
Dreamail <dreaillll@outlook.com>
2025-04-13 14:55:16 +0800

    fix(wechat): remove blur, border and shadow for wechat popup windows

--- modules/gui-apps/default.nix
+++ modules/gui-apps/default.nix
@@ -8,6 +8,14 @@
       (callPackage ./pkgs/qq.nix {})
       (callPackage ./pkgs/wechat.nix {})
     ];
+
+    wayland.windowManager.hyprland.settings = {
+      windowrulev2 = [
+        "noblur, class:wechat, title:wechat"
+        "noborder, class:wechat, title:wechat"
+        "noshadow, class:wechat, title:wechat"
+      ];
+    };
   };
   nixosModule = {inputs, ...}: {};
 }
