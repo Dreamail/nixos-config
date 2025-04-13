13379c416bb5ce25f0534eeffb50441fdc649f19
Dreamail <dreaillll@outlook.com>
2025-04-13 13:49:22 +0800

    fix(gpu): add intel vaapi driver
    this fix the GPU acceleration / starting slowly of Chrome and Electron apps

--- hosts/tnix5p/drivers/gpu.nix
+++ hosts/tnix5p/drivers/gpu.nix
@@ -1,7 +1,12 @@
-{
+{pkgs, ...}: {
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     open = true;
     dynamicBoost.enable = true;
+  };
+
+  hardware.graphics = {
+    extraPackages = [pkgs.intel-vaapi-driver];
+    extraPackages32 = [pkgs.driversi686Linux.intel-vaapi-driver];
   };
 }
