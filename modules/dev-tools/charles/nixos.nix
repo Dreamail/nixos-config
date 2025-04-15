21791b570bb1ecd29daa3a602bdc2fe6ae954179
Dreamail <dreaillll@outlook.com>
2025-04-15 19:32:17 +0800

    feat(charles): hidpi scale

--- modules/dev-tools/charles/nixos.nix
+++ modules/dev-tools/charles/nixos.nix
@@ -1,11 +1,22 @@
 {
   inputs,
-  config,
   pkgs,
   ...
 }:
+let
+  charles-hidpi = pkgs.charles.overrideAttrs (previousAttrs: {
+    nativeBuildInputs = previousAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+    installPhase =
+      previousAttrs.installPhase
+      + ''
+        wrapProgram $out/bin/charles \
+          --set _JAVA_OPTIONS "-Dsun.java2d.uiScale=2" \
+          --run "export XCURSOR_SIZE=\''${HIDPI_XPROP:+\''$((\''${XCURSOR_SIZE} * 2))}"
+      '';
+  });
+in
 {
-  environment.systemPackages = [ pkgs.charles ];
+  environment.systemPackages = [ charles-hidpi ];
 
   security.pki.certificateFiles = [
     "${inputs.mysecrets}/charles-ca/charles-proxy-ssl-proxying-certificate.pem"
