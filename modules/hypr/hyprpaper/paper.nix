41ce12927b6b0e4aa3672d0c64c79fa3e0516b26
Dreamail <dreaillll@outlook.com>
2025-05-02 14:53:48 +0800

    perf(paper): add new wallpaper

--- modules/hypr/hyprpaper/paper.nix
+++ modules/hypr/hyprpaper/paper.nix
@@ -1,6 +1,11 @@
-pkgs: {
-  paper = pkgs.fetchurl {
+pkgs: rec {
+  knd-136-after = pkgs.fetchurl {
     url = "https://static.wikia.nocookie.net/projectsekai/images/8/8c/Ceremony_In_The_Classroom_T.png/revision/latest";
     sha256 = "LkRbGfnoDFhW+K5b3PlDHb9DSarlcitkixSDSTxfMz4=";
   };
+  knd-206-after = pkgs.fetchurl {
+    url = "https://static.wikia.nocookie.net/projectsekai/images/1/13/A_Room_That's_Empty_Now_T.png/revision/latest";
+    sha256 = "gFBF8+S/KKFL/OE7bYejwLt+oMYqn1AD+KMPrGD/6u4=";
+  };
+  paper = knd-206-after;
 }
