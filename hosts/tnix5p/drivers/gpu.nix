3a0d9997125c8779b875f0da1c22a8effaa224fc
Dreamail <dreaillll@outlook.com>
2025-05-25 14:30:34 +0800

    fix(gpu): some apps start slowly
    this fix a that some apps like nautilus may active dGPU and wait dGPU awake when starting
    but this commit did not fix the bug of chromium/electron app

--- hosts/tnix5p/drivers/gpu.nix
+++ hosts/tnix5p/drivers/gpu.nix
@@ -10,4 +10,13 @@
     extraPackages = [ pkgs.intel-media-driver ];
     extraPackages32 = [ pkgs.driversi686Linux.intel-media-driver ];
   };
+
+  boot.initrd.kernelModules = [
+    "i915"
+  ];
+
+  systemd.globalEnvironment = {
+    __EGL_VENDOR_LIBRARY_FILENAMES="${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json:${pkgs.mesa_i686}/share/glvnd/egl_vendor.d/50_mesa.json";
+    VK_ICD_FILENAMES="${pkgs.mesa}/share/vulkan/icd.d/intel_icd.x86_64.json:${pkgs.mesa_i686}/share/vulkan/icd.d/intel_icd.i686.json";
+  };
 }
