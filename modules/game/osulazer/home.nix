0b2100cfe0e1a6e16d66066bb33b46b8cad673b5
Dreamail <dreaillll@outlook.com>
2025-06-27 22:18:28 +0800

    feat(osulazer): add tosu

--- modules/game/osulazer/home.nix
+++ modules/game/osulazer/home.nix
@@ -13,5 +13,6 @@ in
 {
   home.packages = [
     osu-lazer-bin-wayland
+    (pkgs.callPackage ./tosu {})
   ];
 }
---
+++ modules/game/osulazer/tosu/default.nix
@@ -0,0 +1,69 @@
+{
+  fetchFromGitHub,
+  fetchurl,
+  stdenv,
+  makeWrapper,
+  nodejs,
+  pnpm,
+  python3,
+}:
+let
+  nodejs-header = fetchurl {
+    url = "https://nodejs.org/download/release/v${nodejs.version}/node-v${nodejs.version}-headers.tar.gz";
+    sha256 = "sha256-cVrt9kGgJO/e7M1UXOSs3EdZFV4Iwy79/pBpkh/PqGs=";
+  };
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "tosu";
+  version = "4.11.0";
+
+  src = fetchFromGitHub {
+    owner = "tosuapp";
+    repo = "tosu";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-3YDlbs9w3B0Jp+RbLEwkOKY4TQ5WKWg2yj+W5cA738o=";
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    pnpm.configHook
+    python3
+    makeWrapper
+  ];
+
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs)
+      pname
+      version
+      src
+      ;
+    hash = "sha256-sWSj834k6nBczf2iWHTqvFLeNzyPFJwdlYdO3DZ54iE=";
+  };
+
+  env = {
+    NIX_CFLAGS_COMPILE = "-Wno-error=format-security";
+    NPM_CONFIG_TARBALL = "${nodejs-header}";
+  };
+
+  buildPhase = ''
+    pnpm install --frozen-lockfile --no-color --stream
+
+    pnpm run -C packages/tosu genver
+    pnpm run -C packages/tosu ts:compile
+    pnpm run -C packages/tosu compile:prepare-htmls
+  '';
+
+  installPhase = ''
+    mkdir -p $out/share/tosu
+    cp -r packages/tosu/dist/* $out/share/tosu/
+
+    makeWrapper ${nodejs}/bin/node $out/bin/tosu \
+      --add-flags "$out/share/tosu/index.js" \
+      --run "mkdir -p \''${XDG_CONFIG_HOME}/tosu; cd \''${XDG_CONFIG_HOME}/tosu"
+  '';
+
+  fixupPhase = ''
+    substituteInPlace $out/share/tosu/index.js \
+      --replace-fail '__filename,"../../../assets"' '__dirname,"assets"'
+  '';
+})
