8536ed073905d5081dab382790f3a5136f3aab47
Dreamail <dreaillll@outlook.com>
2025-11-04 23:52:38 +0800

    GPU acc for osulazer

--- modules/game/osulazer/home.nix
+++ modules/game/osulazer/home.nix
@@ -7,7 +7,7 @@ let
   osu-lazer-bin-wayland =
     inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override
       (previous: {
-        command_prefix = "env SDL_VIDEODRIVER=wayland ${previous.gamemode}/bin/gamemoderun";
+        command_prefix = "env -u VK_ICD_FILENAMES -u __EGL_VENDOR_LIBRARY_FILENAMES SDL_VIDEODRIVER=wayland __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only VK_ICD_FILENAMES=/nix/store/qh9yzr4hdyq6adjkb1apsi24533czqx3-nvidia-x11-580.76.05-6.15.4/share/vulkan/icd.d/nvidia_icd.x86_64.json";
       });
 in
 {
